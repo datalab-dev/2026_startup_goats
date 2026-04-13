3fee74b10eb9243b931c178aca876c22468a614d
Camila Chicatto <cchicatto@ucdavis.edu>
2026-04-13 11:28:47 -0700

    updating R script but the code for the legs are not showing up like they're supposed to

--- R/legsvisualization.R
+++ R/legsvisualization.R
@@ -1,1 +1,60 @@
 library(tidyverse)
+
+#######
+#DESMOS EQUATIONS
+#######
+
+#left and right leg
+#  l+2>x>l
+# -(l+2)<x<-l
+
+#halfway point through knees
+# y=-r{l+2>x>l}
+# y=-r{-(l+2)<x<-l}
+
+#knee circles
+# (x-(l+1))^{2}+(y+r)^{2}=.75^{2}
+# (x+(l+1))^{2}+(y+r)^{2}=.75^{2}
+
+
+
+#####
+#PLOTTING
+#####
+
+# --- variables where l is width between legs and r is hock (rear knee) height ---
+l <- 2
+r <- 1
+
+# --- knee midline lines ---
+df_segments <- data.frame(
+  x    = c(l, -(l+2)),
+  xend = c(l+2, -l),
+  y    = c(-r, -r),
+  yend = c(-r, -r)
+)
+
+# --- knee circles ---
+theta <- seq(0, 2*pi, length.out = 300)
+
+circle_right <- data.frame(
+  x = (l+1) + 0.75 * cos(theta),
+  y = -r + 0.75 * sin(theta)
+)
+
+circle_left <- data.frame(
+  x = -(l+1) + 0.75 * cos(theta),
+  y = -r + 0.75 * sin(theta)
+)
+
+# --- plot ---
+ggplot() +
+  geom_segment(
+    data = df_segments,
+    aes(x = x, xend = xend, y = y, yend = yend),
+    linewidth = 1
+  ) +
+  geom_path(data = circle_right, aes(x, y), linewidth = 1) +
+  geom_path(data = circle_left, aes(x, y), linewidth = 1) +
+  coord_equal() +
+  theme_minimal()
