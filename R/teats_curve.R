81800cf7f082d8cb4fd21eef6961d62707908e21
Odelyn_Xie <delxie@ucdavis.edu>
2026-04-28 13:39:17 -0700

    added defensive programming to the teats curve

--- R/teats_curve.R
+++ R/teats_curve.R
@@ -8,39 +8,57 @@
 # Right Teat
 # g(x) = h(x - j)(x - (q - j)) - (o + u)
 
-# where:
-# j is the distance from origin or x = 0, does halve the singular teat when j = 0
-# q is the roundness of the teat, the sharpness (this is dependent on the medial)
-# o is where the udder starts from the (medial)
-# o and q are discrete and I think independent, like doesn't affect the teats at all really? Just the placement in the$
-# u is the length of the teat (not mentioned in the rating systems)
-# h is the width of the teat 
-
 library(tidyverse)
 
-teat_model = function(j, q, o, u, h, l, teat_length_score = NULL, n_points = 200) {
+#function with default values 
+
+teat_model = function(
+    teat_placement = 2,
+    depth_of_medial = 3,
+    udder_floor_height = 5,
+    teat_length = 2,
+    teat_diameter = 1,
+    leg_width = 10,
+    teat_length_score = NULL,
+    n_points = 200
+) {
+  
+  # input validation (defensive programming)
+  check_num = function(x, name) {
+    if (is.null(x)) stop(paste(name, "Input is NULL"))
+    if (!is.numeric(x)) stop(paste(name, "Input must be numeric."))
+  }
+  
+  # validate all inputs
+  check_num(teat_placement, "teat_placement")
+  check_num(depth_of_medial, "depth_of_medial")
+  check_num(udder_floor_height, "udder_floor_height")
+  check_num(teat_length, "teat_length")
+  check_num(teat_diameter, "teat_diameter")
+  check_num(leg_width, "leg_width")
+  check_num(n_points, "n_points")
   
 # converting teat scores to inches 
   if (!is.null(teat_length_score)) {
-    if (teat_length_score == 50) u = 5.0
-    else if (teat_length_score == 45) u = 4.5
-    else if (teat_length_score == 40) u = 4.0
-    else if (teat_length_score == 35) u = 3.5
-    else if (teat_length_score == 30) u = 3.0
-    else if (teat_length_score == 25) u = 2.5
-    else if (teat_length_score == 20) u = 2.0
-    else if (teat_length_score == 15) u = 1.5
-    else if (teat_length_score == 10) u = 1.0
-    else if (teat_length_score == 5)  u = 0.5
+    if (teat_length_score == 50) teat_length = 5.0
+    else if (teat_length_score == 45) teat_length = 4.5
+    else if (teat_length_score == 40) teat_length = 4.0
+    else if (teat_length_score == 35) teat_length = 3.5
+    else if (teat_length_score == 30) teat_length = 3.0
+    else if (teat_length_score == 25) teat_length = 2.5
+    else if (teat_length_score == 20) teat_length = 2.0
+    else if (teat_length_score == 15) teat_length = 1.5
+    else if (teat_length_score == 10) teat_length = 1.0
+    else if (teat_length_score == 5)  teat_length = 0.5
   }
   
   #equation 
   
-  x_left  = seq(-l, 0, length.out = n_points)
-  x_right = seq(0, l, length.out = n_points)
+  x_left  = seq(-leg_width, 0, length.out = n_points)
+  x_right = seq(0, leg_width, length.out = n_points)
   
-  y_left  = h * (x_left + j) * (x_left + (q + j)) - (o + u)
-  y_right = h * (x_right - j) * (x_right - (q + j)) - (o + u)
+  y_left  = teat_diameter * (x_left + teat_placement) * (x_left + (depth_of_medial + teat_placement)) - (udder_floor_height + teat_length)
+  y_right = teat_diameter * (x_right - teat_placement) * (x_right - (depth_of_medial + teat_placement)) - (udder_floor_height + teat_length)
   
   df = rbind(
     data.frame(x = x_left,  y = y_left),
@@ -53,3 +71,7 @@ teat_model = function(j, q, o, u, h, l, teat_length_score = NULL, n_points = 200
     coord_fixed(xlim = c(-20, 20), ylim = c(-30, 10)) +
     theme_minimal()
 }
+
+if (sys.nframe() == 0) {
+  teat_model()
+  }
